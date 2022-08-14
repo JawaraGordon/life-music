48fbeeb0cc5730208f3865cf1db1a8a7534cb2a5
JawaraGordon <jawaragordon@icloud.com>
2022-08-13 23:22:12 -0400

    audio player test

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import ReactAudioPlayer from 'react-audio-player';
+import Brunch from '../src/audio/0500Brunch.mp3';
 
 function App() {
   return (
-  <>
-  <Button variant="contained">Hello World</Button>
-  </>
-  )
+    <>
+      <Container maxWidth="sm" style={{ backgroundColor: '#cfe8fc' }}>
+        <h1>Chill Playlist</h1>
 
+        <Button variant="contained">Life Music</Button>
+        <figure>
+          <figcaption>0500 Brunch:</figcaption>
+          <ReactAudioPlayer src={Brunch} autoPlay controls />
+        </figure>
+        <figure>
+          <figcaption>3AM FOMO:</figcaption>
+          <ReactAudioPlayer src="https://jawaragordon.com/music/3AM%20FOMO%20ft.%20Andres.mp3" autoPlay controls />
+        </figure>
+        <figure>
+          <figcaption>0500 Brunch:</figcaption>
+          <ReactAudioPlayer src={Brunch} autoPlay controls />
+        </figure>
+        <figure>
+          <figcaption>0500 Brunch:</figcaption>
+          <ReactAudioPlayer src={Brunch} autoPlay controls />
+        </figure>
+      </Container>
+    </>
+  );
 }
 
 export default App;
