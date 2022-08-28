cdd930af1ef569f386be790199239e9d2d66bc24
JawaraGordon <jawaragordon@icloud.com>
2022-08-28 11:48:49 -0400

    made the header sticky

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,7 @@ import { useHistory } from 'react-router-dom';
 import LifeMusicLogo from '../img/LifeMusicLogo.png';
 import Clock from './Clock';
 
-function Header({ setUser, songs, setSongList, currentMood, 
-  setCurrentMood}) {
+function Header({ setUser, songs, setSongList, currentMood, setCurrentMood }) {
   const history = useHistory();
 
   function handleLogoutClick() {
@@ -16,28 +15,23 @@ function Header({ setUser, songs, setSongList, currentMood,
     });
   }
 
-
-  
-
   return (
     <>
       <div className="header">
-       
-          <a className="header-img" href="/music">
-            <img src={LifeMusicLogo} alt="logo" />
-          </a>
+        <a className="header-img" href="/music">
+          <img src={LifeMusicLogo} alt="logo" />
+        </a>
 
-          <Clock
+        <Clock
           songs={songs}
           setSongList={setSongList}
           currentMood={currentMood}
           setCurrentMood={setCurrentMood}
-          />
-        
+        />
+
         <button onClick={handleLogoutClick} className="header-btn">
           Logout
         </button>
-        
       </div>
     </>
   );
