8fcc67d59b18fb5d607742c7253253c73327eef5
JawaraGordon <jawaragordon@icloud.com>
2025-01-05 14:15:45 -0500

    fix: removed console log from SongList

--- client/src/playlists/SongList.js
+++ client/src/playlists/SongList.js
@@ -1,10 +1,9 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react';
 import Box from '@mui/material/Box';
-import SavePlaylistDialog from '../components/SavePlaylistDialog'
+import SavePlaylistDialog from '../components/SavePlaylistDialog';
 import BasicMenu from '../components/BasicMenu';
 
 function SongList({ songs, user }) {
-
   const [randomSongs, setRandomSongs] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const audioRef = useRef(null);
@@ -16,73 +15,67 @@ function SongList({ songs, user }) {
         .sort(() => Math.random() - 0.5)
         .slice(0, 4);
       setRandomSongs(selectedSongs);
-      console.log('Selected Random Songs:', selectedSongs); 
+      // console.log('Selected Random Songs:', selectedSongs);
     }
   }, [songs]);
- 
+
   const handleSongEnd = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex < randomSongs.length - 1 ? prevIndex + 1 : 0
     );
   };
 
-    // auto-play the next song
-    useEffect(() => {
-      if (audioRef.current) {
-        audioRef.current.play();
-      }
-    }, [currentIndex]);
+  // auto-play the next song
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  }, [currentIndex]);
 
- // playback navigation
- const playPrevious = () => {
-  setCurrentIndex((prevIndex) =>
-    prevIndex > 0 ? prevIndex - 1 : randomSongs.length - 1
-  );
-};
+  // playback navigation
+  const playPrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : randomSongs.length - 1
+    );
+  };
 
-
-const playNext = () => {
-  setCurrentIndex((prevIndex) =>
-    prevIndex < randomSongs.length - 1 ? prevIndex + 1 : 0
-  );
-};
+  const playNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex < randomSongs.length - 1 ? prevIndex + 1 : 0
+    );
+  };
 
   //create song player JSX
   const songPlayer = randomSongs.map((s) => (
-        
     <figure key={s.id}>
       <figcaption>
         <h2>{s.title}</h2>
       </figcaption>
       <div className="song-player">
-          <audio controls loop controlsList="nodownload">
+        <audio controls loop controlsList="nodownload">
           <source src={s.song_url} type="audio/mpeg;" />
         </audio>
       </div>
-      <div>
-      </div>
+      <div></div>
     </figure>
-        
   ));
 
-
-  // function to POST playlist from onClick 
+  // function to POST playlist from onClick
   const savePlayList = () => {
-
     fetch('/playlists', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Accept: 'application/json',
+        Accept: 'application/json'
       },
       body: JSON.stringify({
         user_id: user.id,
-        random_songs: randomSongs}),
-    })
-      
-    };
+        random_songs: randomSongs
+      })
+    });
+  };
 
-  if (!songPlayer.length ) return 
+  if (!songPlayer.length) return;
 
   return (
     <>
